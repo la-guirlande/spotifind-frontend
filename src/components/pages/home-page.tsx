26536948751be7d91bf05c4de36aec3b300f4361
Lelberto <jsurieux@gmail.com>
2021-10-11 19:54:29 +0200

    Add router

---
+++ src/components/pages/home-page.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+
+/**
+ * Home page component.
+ */
+export const HomePage: FC = () => (
+  <p>Hello World</p>
+);
---
+++ src/components/router.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HomePage } from './pages/home-page';
+
+/**
+ * Router component.
+ */
+export const Router: FC = () => (
+  <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+);
