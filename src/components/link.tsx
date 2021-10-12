a119bdfffbccad1993b17510a0ddbf22febb507f
Lelberto <jsurieux@gmail.com>
2021-10-12 04:32:48 +0200

    Add link component

---
+++ src/components/link.tsx
@@ -0,0 +1,44 @@
+import { AnchorHTMLAttributes, FC } from 'react';
+import { Link as ReactRouterLink, LinkProps as ReactRouterLinkProps } from 'react-router-dom';
+import { Variant } from '../types/styles';
+
+/**
+ * Link props.
+ */
+export interface LinkProps extends AnchorHTMLAttributes<HTMLAnchorElement>, ReactRouterLinkProps {
+  variant?: Variant;
+}
+
+/**
+ * Link component. 
+ */
+export const Link: FC<LinkProps> = ({ variant, children, ...props }) => {
+  let className;
+  switch (variant) {
+    default:
+    case 'primary':
+      className = 'text-primary hover:text-primary-light';
+      break;
+    case 'secondary':
+      className = 'text-secondary hover:text-secondary-light';
+      break;
+    case 'light':
+    case 'dark':
+      className = 'text-dark dark:text-light';
+      break;
+    case 'info':
+      className = 'text-info hover:text-info-light';
+      break;
+    case 'warn':
+      className = 'text-warn hover:text-warn-light';
+      break;
+    case 'error':
+      className = 'text-error hover:text-error-light';
+      break;
+  }
+  return <ReactRouterLink className={`px-8 py-1 rounded-full font-gotham-bold transform hover:scale-105 focus:scale-100 transition duration-50 ${className}`} {...props}>{children}</ReactRouterLink>
+}
+
+Link.defaultProps = {
+  variant: 'primary'
+}
