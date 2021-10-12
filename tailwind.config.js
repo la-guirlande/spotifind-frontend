8ba392dbf61bb543d68fe47c8d58a3918c096f55
Lelberto <jsurieux@gmail.com>
2021-10-12 02:49:55 +0200

    Add Gotham fonts and set Gotham-Medium to default font Add some Tailwind variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,35 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'gotham-black': ['Gotham-Black'],
+        'gotham-medium': ['Gotham-Medium'],
+        'gotham-book': ['Gotham-Book'],
+        'gotham-bold': ['Gotham-Bold']
+      },
+      colors: {
+        primary: '#6900FF',
+        secondary: '#FFD700',
+        light: '#FAF7FF',
+        dark: '#41236D',
+        info: '#14D4FF',
+        warn: '#FFAA17',
+        error: '#D63E13'
+      },
+      borderWidth: {
+        '1': '1px'
+      },
+      transitionDuration: {
+        '50': '50ms'
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      borderWidth: ['hover', 'focus']
+    },
   },
   plugins: [],
 }
