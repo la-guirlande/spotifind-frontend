05fbddab0331cbb21414f6773768d43f3ee6f9ac
Lelberto <jsurieux@gmail.com>
2021-10-15 18:03:42 +0200

    Remove refresh token in local storage

--- src/util/local-storage.ts
+++ src/util/local-storage.ts
@@ -3,7 +3,6 @@
  */
 export enum LocalStorageKey {
   ACCESS_TOKEN = 'access-token',
-  REFRESH_TOKEN = 'refresh-token',
   SPOTIFY_ACCESS_TOKEN = 'spotify-access-token',
   SPOTIFY_REFRESH_TOKEN = 'spotify-refresh-token',
   OAUTH2_STATE = 'oauth2-state',
