63537fa1b1cafeb18e131dccae40d580229ac546
Lelberto <jsurieux@gmail.com>
2021-10-18 17:48:47 +0200

    Update home page positionment

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -6,17 +6,17 @@ import { MainView } from '../views/main-view';
  * Home page component.
  */
 export const HomePage: FC = () => (
-  <div className="h-screen grid grid-rows-2 grid-cols-1 md:grid-cols-3 justify-items-stretch">
-    <div className="hidden md:inline bg-white dark:bg-black">
+  <div className="h-screen grid grid-rows-3 grid-cols-1 md:grid-cols-5 auto-cols-max justify-items-stretch">
+    <div className="hidden md:inline row-span-2 bg-white dark:bg-black">
       <Sidebar />
     </div>
-    <div className="bg-gray-300 dark:bg-gray-700">
+    <div className="row-span-2 col-span-3 bg-gray-300 dark:bg-gray-700">
       <MainView />
     </div>
-    <div className="hidden md:block">
+    <div className="hidden md:block row-span-2">
       Right view
     </div>
-    <div className="md:col-span-3">
+    <div className="col-span-full">
       Down view
     </div>
   </div>
